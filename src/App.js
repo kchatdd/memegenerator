aaab63317cec0c70ab68b1f119272afe357b2c97
kchatdd <62733349+kchatdd@users.noreply.github.com>
2024-05-09 20:10:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+
+const App =()=> {
+
+    return (
+            <>
+                <Header />
+                <Meme />
+            </>
+        )
+
+}
+
+export default App
---
+++ src/components/Meme.js
@@ -0,0 +1,24 @@
+const Meme =()=> {
+
+    return (
+        <main>
+                <form className="form">
+                        <input 
+                        type="text"
+                        placeholder="top-text"
+                        className="form-input"
+                        />
+                        <input 
+                        type="text"
+                        placeholder="bottom-text"
+                        className="form-input"
+                        />
+                    <button className="form-button">
+                        Get a new meme image
+                    </button>
+                </form>
+        </main>
+    )
+}
+
+export default Meme
